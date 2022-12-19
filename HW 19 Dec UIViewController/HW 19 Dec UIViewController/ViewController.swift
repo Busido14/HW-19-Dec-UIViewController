22a27dd989439bf83d5472b16317790b7da89249
Artem <artchzhen@gmail.com>
2022-12-19 20:04:38 +0500

    create tableView information on UIViewController

--- HW 19 Dec UIViewController/HW 19 Dec UIViewController/ViewController.swift
+++ HW 19 Dec UIViewController/HW 19 Dec UIViewController/ViewController.swift
@@ -8,10 +8,41 @@
 import UIKit
 
 class ViewController: UITableViewController {
-
+    var nameUIViewControllers = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-    
+        title = "UIViewController"
+        navigationItem.largeTitleDisplayMode = .never
+        
+        let fm = FileManager.default
+            let path = Bundle.main.resourcePath!
+            let items = try! fm.contentsOfDirectory(atPath: path)
+            
+        for item in items {
+            if item.hasSuffix("().png") {
+                nameUIViewControllers.append(item)
+            }
+        }
+        
+    }
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return nameUIViewControllers.count
+        
+    }
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Name", for: indexPath)
+        cell.textLabel?.text = nameUIViewControllers[indexPath.row].replacingOccurrences(of: ".png", with: "")
+//        cell.imageView?.image = UIImage(named: nameUIViewControllers[indexPath.row])
+        return cell
+    }
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        if let vc = storyboard?.instantiateViewController(withIdentifier: "Two") as? ViewControllerTwo {
+            vc.selectedImage = nameUIViewControllers[indexPath.row]
+//            vc.textLabel?.text = nameUIViewControllers[indexPath.row]
+//            vc.selectedWebsite = indexPath.row
+            navigationController?.pushViewController(vc, animated: true)
+        }
     }
 }
-
---
+++ HW 19 Dec UIViewController/HW 19 Dec UIViewController/ViewControllerTwo.swift
@@ -0,0 +1,27 @@
+//
+//  ViewControllerTwo.swift
+//  HW 19 Dec UIViewController
+//
+//  Created by Артем Чжен on 19/12/22.
+//
+
+import UIKit
+
+class ViewControllerTwo: UIViewController {
+    @IBOutlet var imageView: UIImageView!
+    
+    var selectedImage: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        title = "informition of \(selectedImage!.replacingOccurrences(of: ".png", with: ""))"
+        navigationController?.navigationBar.prefersLargeTitles = true
+        
+        if let imageToLoad = selectedImage {
+            
+            imageView.image = UIImage(named: imageToLoad)
+            imageView.layer.borderWidth = 1
+        }
+    }
+}
