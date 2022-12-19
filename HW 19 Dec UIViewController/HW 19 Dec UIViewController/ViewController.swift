c5be4e6c84535a5c06937b7dbedfc73abea5e23d
Artem <artchzhen@gmail.com>
2022-12-19 16:47:27 +0500

    intional devolopment

---
+++ HW 19 Dec UIViewController/HW 19 Dec UIViewController/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  HW 19 Dec UIViewController
+//
+//  Created by Артем Чжен on 19/12/22.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+}
+
