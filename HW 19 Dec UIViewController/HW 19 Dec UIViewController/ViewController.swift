302a6717383b2b62ce773e8669b1b43cf8ca8fa0
Artem <artchzhen@gmail.com>
2022-12-21 16:22:30 +0500

    change name

--- HW 19 Dec UIViewController/HW 19 Dec UIViewController/ViewController.swift
+++ HW 19 Dec UIViewController/HW 19 Dec UIViewController/ViewController.swift
@@ -8,7 +8,7 @@
 import UIKit
 
 class ViewController: UITableViewController {
-    var nameUIViewControllers =  ["init()", "loadView()", "viewDidLoad()", "viewWillAppear()", "viewDidAppear()", "viewWillDisappear()", "viewDidDisappear()", "viewDidUnload()"]
+    var nameUIViewController =  ["init()", "loadView()", "viewDidLoad()", "viewWillAppear()", "viewDidAppear()", "viewWillDisappear()", "viewDidDisappear()", "viewDidUnload()"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,18 +17,18 @@ class ViewController: UITableViewController {
 
     }
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return nameUIViewControllers.count
+        return nameUIViewController.count
        
     }
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "Name", for: indexPath)
-        cell.textLabel?.text = nameUIViewControllers[indexPath.row].replacingOccurrences(of: ".png", with: "")
+        cell.textLabel?.text = nameUIViewController[indexPath.row].replacingOccurrences(of: ".png", with: "")
         return cell
       
     }
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         if let vc = storyboard?.instantiateViewController(withIdentifier: "Two") as? ViewControllerTwo {
-            vc.selectedImage = nameUIViewControllers[indexPath.row]
+            vc.selectedImage = nameUIViewController[indexPath.row]
            
             navigationController?.pushViewController(vc, animated: true)
         }
